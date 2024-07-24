b503fa0f6b74dd6af98d9c090172c5930fc69fac
alexg <alex@zifferbilt.com>
2024-07-24 02:27:43 +0000

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch('/')
+      .then(response => response.text())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Zifferbilt</h1>
       </header>
     </div>
   );
 }
 
 export default App;
+
